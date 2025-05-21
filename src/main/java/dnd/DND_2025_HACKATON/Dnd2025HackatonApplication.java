905274ea34abded774b48f6fce98a4e516072307
Shin Jihoon <tobeabestns@naver.com>
2025-05-21 21:20:37 +0900

    Init

---
+++ src/main/java/dnd/DND_2025_HACKATON/Dnd2025HackatonApplication.java
@@ -0,0 +1,13 @@
+package dnd.DND_2025_HACKATON;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dnd2025HackatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dnd2025HackatonApplication.class, args);
+	}
+
+}
